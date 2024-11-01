10699183165bf5e3a4dc54f14e861d1473afe164
hihilong <hyelong20630@gmail.com>
2024-11-01 22:53:23 +0900

    [feat] 프로젝트 구조 형성

---
+++ src/app/firestore/firestore.ts
@@ -0,0 +1,9 @@
+import firebase from './firebase';
+import {getFirestore} from 'firebase/firestore';
+
+const fireStore = getFirestore(firebase);
+export default fireStore
+
+
+//addDoc  await addDoc(collection(firestore,"users"),{
+//getDoc  await getDoc(collection(firestore, "users"),{ ~정보});
