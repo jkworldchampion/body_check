129930a6dd867e51d4424cc3094ffc3fe7053ee9
hihilong <hyelong20630@gmail.com>
2024-11-14 14:53:55 +0900

    refactor : 이메일 인증방식 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,22 @@
-import Link from 'next/link';
 
-
+import HeroSection from './pages/HeroSection';
+import WhySection from './pages/WhySection';
+import RecordSection from './pages/RecordSection';
+import GraphSection from "./pages/GraphSection";
+import BodySystemSection from './pages/BodySystemSection';
+import TogetherSection from './pages/TogetherSection';
+import Footer from '././componenets/Footer'
 
 export default function Home() {
     return (
-        <div style={{textAlign:'left',marginTop:'300px',marginLeft:'50px'}}>
-            <Link href="/">
-                <button>BODY:CHECK</button>
-            </Link>
-            <Link href="/login">
-                <button>로그인</button>
-            </Link>
-            <Link href="/inquiry">
-                <button>문의사항</button>
-            </Link>
-            <h1 style={{fontSize:'40px'}}>체형맞춤형 운동추천</h1>
-            <h2 style={{fontSize:'100px',fontWeight:'bold'}}>BODY CHECK</h2>
-            <Link href="/start" >
-                <button >서비스 시작하기</button>
-            </Link>
-
-
-
-        </div>
+        <>
+            <HeroSection />
+            <WhySection />
+            <RecordSection />
+            <GraphSection />
+            <BodySystemSection />
+            <TogetherSection />
+            <Footer />
+        </>
     );
 }
