2bd5fd42c2615f95612a10f70aea28e5b277bddc
hihilong <hyelong20630@gmail.com>
2024-11-15 07:25:27 +0900

    feature : zustand 기본세팅 + 회원가입 상태관리 store

---
+++ src/store/useSignupStore.ts
@@ -0,0 +1,36 @@
+// src/store/useSignupStore.ts
+import { create } from "zustand";
+
+interface SignupFormState {
+    id: string;
+    password: string;
+    confirmPassword: string;
+    name: string;
+    gender: string;
+    address: string;
+    isIdUnique: boolean | null;
+    isSignupComplete: boolean;
+    setField: (field: keyof SignupFormState, value: string | boolean | null) => void;
+    passwordError: string | null;          // 비밀번호 오류 메시지
+    confirmPasswordError: string | null;    // 비밀번호 확인 오류 메시지
+}
+
+const useSignupStore = create<SignupFormState>((set) => ({
+    id: "",
+    password: "",
+    confirmPassword: "",
+    name: "",
+    gender: "",
+    address: "",
+    isIdUnique: null,
+    isSignupComplete: false,
+    passwordError: null,
+    confirmPasswordError: null,
+    setField: (field, value) =>
+        set((state) => ({
+            ...state,
+            [field]: value,
+        })),
+}));
+
+export default useSignupStore;
