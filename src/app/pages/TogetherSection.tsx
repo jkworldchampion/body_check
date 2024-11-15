af09fb912c55887a7807e867829544a77faf258f
hihilong <hyelong20630@gmail.com>
2024-11-15 18:12:18 +0900

    feature : together section 페이지 # <타입> : <제목> 형식으로 작성하며 제목은 최대 50글자 정도로만 입력 # 제목을 아랫줄에 작성, 제목 끝에 마침표 금지, 무엇을 했는지 명확하게 작성
    ################
    # 본문(추가 설명)을 아랫줄에 작성
    
    ################
    # 꼬릿말(footer)을 아랫줄에 작성 (관련된 이슈 번호 등 추가)
    
    ################
    # feature : 새로운 기능 추가
    # fix : 버그 수정
    # docs : 문서 수정
    # test : 테스트 코드 추가
    # refactor : 코드 리팩토링
    # style : 코드 의미에 영향을 주지 않는 변경사항
    # chore : 빌드 부분 혹은 패키지 매니저 수정사항
    ################

--- src/app/pages/TogetherSection.tsx
+++ src/app/pages/TogetherSection.tsx
@@ -1,18 +1,30 @@
-
-import React from "react";
+'use client';
+import React, { useRef } from "react";
 import styles from "./TogetherSection.module.css";
+import { useObserver } from "../utils/useObserver";
 
 export default function TogetherSection() {
+    const textRef = useRef(null);
+    const imageRef = useRef(null);
+    const { isVisible: isTextVisible } = useObserver({ target: textRef, option: { threshold: 0.3 } });
+    const { isVisible: isImageVisible } = useObserver({ target: imageRef, option: { threshold: 0.5 } });
+
     return (
         <div className={styles.section}>
-            <div className={styles.textContainer}>
+            <div
+                ref={textRef}
+                className={`${styles.textContainer} ${isTextVisible ? styles.fadeIn : ""}`}
+            >
                 <h2 className={styles.title}>TOGETHER <br /> JOIN</h2>
                 <p className={styles.description}>
                     공유하기 기능을 통해 친구들과 함께 나의 기록을 자랑하고
                     나의 상대적 위치를 확인할 수 있어요.
                 </p>
             </div>
-            <div className={styles.imageContainer}>
+            <div
+                ref={imageRef}
+                className={`${styles.imageContainer} ${isImageVisible ? styles.slideIn : ""}`}
+            >
                 <img
                     src="/images/gotocellphone.png"
                     alt="친구와 함께하는 이미지"
