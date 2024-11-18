e569c468d4a87addfc32ac693668704abbf44426
hihilong <hyelong20630@gmail.com>
2024-11-18 20:13:38 +0900

    refactor : 로그인 url 수정, 회원가입 url 수정

---
+++ src/store/useSignupStore.tsx
@@ -0,0 +1,58 @@
+import { create } from "zustand";
+
+interface SignupFormState {
+    id: string;
+    password: string;
+    confirmPassword: string;
+    name: string;
+    gender: string;
+    address: string;
+    isIdUnique: boolean | null;
+    isSignupComplete: boolean;
+    passwordError: string | null;          // 비밀번호 오류 메시지
+    confirmPasswordError: string | null;  // 비밀번호 확인 오류 메시지
+    setField: <T extends keyof SignupFormState>(field: T, value: SignupFormState[T]) => void;
+    validatePasswords: () => void;        // 비밀번호 유효성 검사 함수
+}
+
+const useSignupStore = create<SignupFormState>((set) => ({
+    id: "",
+    password: "",
+    confirmPassword: "",
+    name: "",
+    gender: "",
+    address: "",
+    isIdUnique: null,
+    isSignupComplete: false,
+    passwordError: null,
+    confirmPasswordError: null,
+    setField: <T extends keyof SignupFormState>(field: T, value: SignupFormState[T]) =>
+        set((state) => ({
+            ...state,
+            [field]: value,
+        })),
+    validatePasswords: () =>
+        set((state) => {
+            const passwordError = state.password.length < 6 ? "비밀번호는 6자 이상이어야 합니다." : null;
+            const confirmPasswordError =
+                state.password !== state.confirmPassword ? "비밀번호가 일치하지 않습니다." : null;
+
+            return {
+                ...state,
+                passwordError,
+                confirmPasswordError,
+            };
+        }),
+}));
+
+interface VisibilityState {
+    isVisible: boolean;
+    setIsVisible: (visible: boolean) => void;
+}
+
+export const useVisibilityStore = create<VisibilityState>((set) => ({
+    isVisible: false,
+    setIsVisible: (visible) => set({ isVisible: visible }), // 공백 수정
+}));
+
+export default useSignupStore;
