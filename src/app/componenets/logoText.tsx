1425a33b062dab471282ff3f8fc5b9ec9723c033
hihilong <hyelong20630@gmail.com>
2024-12-01 17:12:04 +0900

    style : 스타일 수정

--- src/app/componenets/logoText.tsx
+++ src/app/componenets/logoText.tsx
@@ -14,7 +14,7 @@ const LogoText: React.FC<LogoTextProps> = ({ text }) => {
 
 const styles = {
     logoText: {
-        color: '#0033CC',      // 진한 파란색
+        color: '#030303',
         fontWeight: 'bold' as const, // 굵은 텍스트
         fontSize: '24px',      // 필요에 따라 글자 크기 조정 가능
         fontFamily: 'Arial, sans-serif', // 기본 폰트 (필요에 따라 변경 가능)
